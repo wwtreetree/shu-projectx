1dfeab8ee8b35c182c0564edb1ad3d6a1f4c715e
shuwang <o2h2b@ugrad.cs.ubc.ca>
2020-09-06 13:59:56 -0700

    9/6

--- src/data_handler.py
+++
@@ -1,4 +0,0 @@
-def get_dataset(args):
-    # place conditionals here
-    data = 1
-    return data
--- src/model_handler.py
+++
@@ -1,4 +0,0 @@
-def get_model(args):
-    # place conditionals here
-    model = 1
-    return model
